44c3067c1ae26b7de664c7bce9fe38aa586092b0
Nafisa Schofield <schofield.n.nyc@gmail.com>
2018-06-23 22:24:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
 var height = 40
-var message = `${name} height is  less than ${height}`
+var message = `${name} height is  greater than ${height}`
 
 module.exports = {name, height, message}
